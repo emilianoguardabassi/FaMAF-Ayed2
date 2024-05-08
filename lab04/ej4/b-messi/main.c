4a7922871b943409b694b6bd597beff237a9fe57
Emiliano Guardabassi <emilianojguardabassi@hotmail.com>
2024-05-08 17:01:00 -0300

    Lab04/ej4/b-messi done

--- lab04/ej4/b-messi/main.c
+++ lab04/ej4/b-messi/main.c
@@ -3,26 +3,29 @@
 
 #include "data.h"
 
-void
-print_data(data_t d) {
-    printf("NOMBRE: %s\n"
-           "EDAD  : %d años\n"
-           "ALTURA: %d cm\n\n",
-           d.name, d.age, d.height);
+void print_data(data_t d) {
+  printf("NOMBRE: %s\n"
+         "EDAD  : %d años\n"
+         "ALTURA: %d cm\n\n",
+         d.name, d.age, d.height);
 }
 
 void set_name(name_t new_name, data_t *d) {
-    /* COMPLETAR */
+  unsigned int i = 0u;
+  while (new_name[i] != '\0') {
+    d->name[i] = new_name[i];
+    i++;
+  }
+  // d->name[i] = '\0';
 }
 
 int main(void) {
 
-    data_t messi = {"Leo Messi", 36, 169};
-    print_data(messi);
-    name_t messi_full_name = "Lionel Messi";
-    /* COMPLETAR */
-    print_data(messi);
+  data_t messi = {"Leo Messi", 36, 169};
+  print_data(messi);
+  name_t messi_full_name = "Lionel Messi";
+  set_name(messi_full_name, &messi);
+  print_data(messi);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
-
